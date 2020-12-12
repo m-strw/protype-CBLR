718960cabf405c7ac5684797acb821aa2beb53a1
m-strw <69999281+m-strw@users.noreply.github.com>
2020-12-12 16:49:20 +1300

    setup

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Text } from 'react-native'
+
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+// Screen
+import AddScreen from './components/Main/Add'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+import * as firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD15ryfnhRk-XmGy97ST8QeSDsHQLSZN1E",
+  authDomain: "instagram-dev-b3430.firebaseapp.com",
+  databaseURL: "https://instagram-dev-b3430.firebaseio.com",
+  projectId: "instagram-dev-b3430",
+  storageBucket: "instagram-dev-b3430.appspot.com",
+  messagingSenderId: "830735457757",
+  appId: "1:830735457757:web:17aa2c73368adf7ab8aa34",
+  measurementId: "G-ZY1HKC7QX9"
+};
+// for production use ennvirment variables
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+         <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={ MainScreen } options={{ headerShown: false }} />
+            <Stack.Screen name="Add" component={AddScreen} />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+
+      </Provider>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
+
+
+
+
---
+++ components/Main.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from  'react-native-vector-icons/MaterialCommunityIcons' 
+
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchUser } from '../redux/actions/index'
+
+import FeedScreen from './main/Feed'
+import ProfileScreen from './main/Profile'
+import { event } from 'react-native-reanimated';
+
+const EmptyScreen = () =>{
+    return(null)
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+export class Main extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() { 
+        return(       
+          <Tab.Navigator initialRouteName="Feed" labeled = {false} >
+              <Tab.Screen name="Feed" component={FeedScreen} options={{
+                  tabBarIcon: ({ colour, size }) => (
+                      <MaterialCommunityIcons name = "home" colour ={colour}
+                      size = {26} />
+                  )
+              }} />
+                               <Tab.Screen name="AddContainer" component={EmptyScreen}
+                    listeners={({ navigation }) => ({
+                        tabPress: event => {
+                            event.preventDefault();
+                            navigation.navigate("Add")
+                        }
+                    })}
+                    options={{
+                        tabBarIcon: ({ colour, size }) => (
+                            <MaterialCommunityIcons name="format-paint" colour={colour} size={26} />
+                        ),
+                    }} />
+                 <Tab.Screen name="Profile" component={ProfileScreen} options={{
+                  tabBarIcon: ({ colour, size }) => (
+                      <MaterialCommunityIcons name = "account-circle" colour ={colour}
+                      size = {26} />
+                  )
+              }} />
+          </Tab.Navigator>
+        )
+    }
+}
+
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser
+})
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchProps)(Main)
